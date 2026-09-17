6d61b3495fde84fdfd344bc2a6d93a82de803038
justinevilocura <justinevilocura12@gmail.com>
2026-09-18 05:24:36 +0800

    feat(web): update android apk download link to build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -351,7 +351,7 @@ const Home = () => {
 
                   <div className="space-y-3">
                     <a
-                      href="https://expo.dev/accounts/jscvilocura/projects/somnia/builds/250d17a6-28fb-4975-8ebb-89f155b397bd"
+                      href="https://expo.dev/accounts/jscvilocura/projects/somnia/builds/58e5f5a4-baa0-4197-b136-782683bdc4b9"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="w-full py-3 px-4 bg-gradient-to-r from-blue-600 to-cyan-600 hover:from-blue-500 hover:to-cyan-500 text-white rounded-xl font-medium text-sm flex items-center justify-center space-x-2 transition-all shadow-lg shadow-blue-500/20"
