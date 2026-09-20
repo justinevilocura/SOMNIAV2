6cd42807e54f0dc4da61e51b7091083e4e4b15ce
justinevilocura <justinevilocura12@gmail.com>
2026-09-20 19:25:10 +0800

    updated link download for app

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -351,7 +351,7 @@ const Home = () => {
 
                   <div className="space-y-3">
                     <a
-                      href="https://expo.dev/accounts/jscvilocura/projects/somnia/builds/9ddd1fc7-f38d-4e76-90ba-c92fb74f84a5"
+                      href="https://expo.dev/accounts/jscvilocura/projects/somnia/builds/7d6c909c-55e3-4049-897a-5d9f4bfde96e"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="w-full py-3 px-4 bg-gradient-to-r from-blue-600 to-cyan-600 hover:from-blue-500 hover:to-cyan-500 text-white rounded-xl font-medium text-sm flex items-center justify-center space-x-2 transition-all shadow-lg shadow-blue-500/20"
